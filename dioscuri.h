edbe1a2296821d3ddf205f1615c621cd2899fea9
Raul Rincon <raulrincon@gmail.com>
2025-04-13 20:25:01 -0400

    updated some files

---
+++ dioscuri.h
@@ -0,0 +1,92 @@
+/* Dioscuri Config
+╭───────────────────────────┬────────────────────────────╮ ╭─────────────────────────────┬─────────────────────────────╮
+|  0   1   2   3   4   5  6 |  7   8   9  10  11  12  13 | | LN6 LN5 LN4 LN3 LN2 LN1 LN0 | RN0 RN1 RN2 RN3 RN4 RN5 RN6 |
+| 14  15  16  17  18  19    | 20  21  22  23  24  25  26 | | LT5 LT4 LT3 LT2 LT1 LT0	 | RT0 RT1 RT2 RT3 RT4 RT5 RT6 |
+| 27  28  29  30  31  32	| 33  34  35  36  37  38  39 | | LM5 LM4 LM3 LM2 LM1 LM0	 | RM0 RM1 RM2 RM3 RM4 RM5 RM6 |
+| 40  41  42  43  44  45	| 46  47  48  49  50  51  52 | | LB5 LB4 LB3 LB2 LB1 LB0	 | RB0 RB1 RB2 RB3 RB4 RB5 RB6 |
+| 53  54		  55  56	| 57  58	  	  59  60  61 | | LH3 LH2		 LH1 LH0	 | RH0 RH1 		   RH2 RH3 RH4 |
+╰───────────────────────────┴────────────────────────────╯ ╰─────────────────────────────┴─────────────────────────────╯ */
+
+#pragma once
+
+// Number Left Row
+#define LN0  6
+#define LN1  5
+#define LN2  4
+#define LN3  3
+#define LN4  2
+#define LN5  1
+#define LN6  0
+
+// Number Right Row
+#define RN0  7
+#define RN1  8
+#define RN2  9
+#define RN3  10
+#define RN4  11
+#define RN5  12
+#define RN6  13
+
+// Top Left Row
+#define LT0  19
+#define LT1  18
+#define LT2  17
+#define LT3  16
+#define LT4  15
+#define LT5  14
+
+// Top Right Row
+#define RT0  20
+#define RT1  21
+#define RT2  22
+#define RT3  23
+#define RT4  24
+#define RT5  25
+#define RT6  26
+
+// Middle Left Row
+#define LM0  32
+#define LM1  31
+#define LM2  30
+#define LM3  29
+#define LM4  28
+#define LM5  27
+
+// Middle Right Row
+#define RM0  33
+#define RM1  34
+#define RM2  35
+#define RM3  36
+#define RM4  37
+#define RM5  38
+#define RM6  39
+
+// Bottom Left Row
+#define LB0  45
+#define LB1  44
+#define LB2  43
+#define LB3  42
+#define LB4  41
+#define LB5  40
+
+// Bottom Right Row
+#define RB0  46
+#define RB1  47
+#define RB2  48
+#define RB3  49
+#define RB4  50
+#define RB5  51
+#define RB6  52
+
+// Thumb Left Row
+#define LH0  56
+#define LH1  55
+#define LH2  54
+#define LH3  53
+
+// Thumb Right Row
+#define RH0  57
+#define RH1  58
+#define RH2  59
+#define RH3  60
+#define RH4  61
